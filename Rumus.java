bc68f97cec3a0716a3f2d3a3f6d3c10f1c1612e7
Mrl4n0r <lanamau368@gmail.com>
2025-06-12 12:38:57 +0800

    membuat accesor dan constructor

--- Rumus.java
+++ Rumus.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan3;
 
-/**
- *
- * @author Lenovo LOQ
- */
+// Nama  : Muhammad Maulana
+// NPM   : 2310010375
+// Kelas : 4C
+
 public class Rumus {
-    
+    int sisi;
+    int panjang;
+    int lebar;
+    int hasil;
+
+    public Rumus() {
+        sisi = 0;
+        panjang = 0;
+        lebar = 0;
+        hasil = 0;
+    }
+
+    public void hitungLuasPersegi() {
+        hasil = sisi * sisi;
+        System.out.println("Luas Persegi = " + hasil);
+    }
+
+    public void hitungLuasPersegiPanjang() {
+        hasil = panjang * lebar;
+        System.out.println("Luas Persegi Panjang = " + hasil);
+    }
+
+    public int getHasil() {
+        return hasil;
+    }
 }
