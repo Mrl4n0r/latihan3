bb382d9668b2f352f2e71c106cec33ecdc5edfae
Mrl4n0r <lanamau368@gmail.com>
2025-06-12 12:39:44 +0800

    Membuat class main

--- RumusBeraksi.java
+++ RumusBeraksi.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan3;
 
-/**
- *
- * @author Lenovo LOQ
- */
 public class RumusBeraksi {
-    
+    public static void main(String[] args) {
+
+        Rumus maul = new Rumus();
+
+        maul.sisi = 5;
+        maul.hitungLuasPersegi();
+        System.out.println("Hasil luas persegi (akses via getHasil): " + maul.getHasil());
+
+        maul.panjang = 7;
+        maul.lebar = 3;
+        maul.hitungLuasPersegiPanjang();
+        System.out.println("Hasil luas persegi panjang (akses via getHasil): " + maul.getHasil());
+    }
 }
